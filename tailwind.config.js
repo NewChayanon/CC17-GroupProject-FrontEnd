e6f9defc53c20fadb102cc62c9f70d97af380c04
Vasin Thepsoparn <vasindevwork@gmail.com>
2024-06-23 16:36:40 +0700

    tailwind config FF v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,30 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#20831E",
+        secondary: "#FFD850",
+        tertiary: "#0806300",
+        graybg: "#F2F2F2",
+        graylighttext: "#8D8D8D",
+        graydarktext: "#545454",
+        graylighticon: "#CCCCCC",
+        absolutewhite: "#FFFFFF",
+        absoluteblack: "#000000",
+        darkgreen: "#1F6F1D",
+        vividgreen: "#20831E",
+        lightgreen: "#2E9D2B",
+        darkyellow: "#EBC12B",
+        vividyellow: "#FFD850",
+        lightyellow: "#FDE078",
+        darkbrown: "#695200",
+        vividbrown: "#0806300",
+        lightbrown: "#AC8704",
+      },
+    },
   },
-  plugins: [
-    require('daisyui'),
-],
-}
-
+  plugins: [require("daisyui")],
+};
