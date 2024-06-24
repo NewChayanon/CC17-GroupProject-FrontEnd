27feb9b0a70d9fc7314ef7e1c88394086677df6d
kulhaka <157956346+kulhaka@users.noreply.github.com>
2024-06-24 11:34:14 +0700

    Update NavMenu.jsx

--- src/components/NavMenu.jsx
+++ src/components/NavMenu.jsx
@@ -1,8 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function NavMenu({ menuIcon, menuName, linkTo }) {
-  console.log();
+export default function NavMenu({ menuIcon, menuName, linkTo = "/" }) {
   return (
     <li className="p-2.5">
       <Link to={linkTo}>
