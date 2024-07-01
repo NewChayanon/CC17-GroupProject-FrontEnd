4a4975177a74c96c41f9b2a0782b7b42f45d5f00
SiraphobCh <spcc.321@gmail.com>
2024-07-01 15:00:57 +0700

    allign sidebar and finish admin sidebar

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
-export default function SidebarMenu({ menuIcon, menuName, linkTo = "/" }) {
+export default function SidebarMenu({ menuIcon, menuName, linkTo = '/' }) {
   const { pathname } = useLocation();
   const [isActive, setIsActive] = useState(false);
   useEffect(() => {
@@ -11,8 +11,13 @@ export default function SidebarMenu({ menuIcon, menuName, linkTo = "/" }) {
   }, []);
   return (
     <li className={`p-2`}>
-      <a className={`p-2 ${isActive && "bg-lightyellow"}`} href={linkTo}>
-        {menuIcon}
+      <a
+        className={`p-2 flex items-center space-x-2 ${isActive && 'bg-lightyellow'}`}
+        href={linkTo}
+      >
+        <div className="flex-shrink-0 w-[25px] h-[25px] flex items-center justify-center">
+          {menuIcon}
+        </div>
         <p className="text-sm">{menuName}</p>
       </a>
     </li>
--- src/layouts/Sidebar.jsx
+++ src/layouts/Sidebar.jsx
@@ -1,5 +1,5 @@
-import SidebarMenu from "../components/SidebarMenu";
-import { useLocation } from "react-router-dom";
+import SidebarMenu from '../components/SidebarMenu';
+import { useLocation } from 'react-router-dom';
 import {
   StoreIcon,
   InboxIcon,
@@ -8,8 +8,12 @@ import {
   MyFavoriteSeller,
   VoucherListIcon,
   CalendarIcon,
-} from "../icons/index.jsx";
-import useStore from "../zustand/store.js";
+  UserSummary,
+  BuyerIcon,
+  ReportIcon,
+  InboxInbox,
+} from '../icons/index.jsx';
+import useStore from '../zustand/store.js';
 
 export default function Sidebar() {
   const { pathname } = useLocation();
@@ -17,79 +21,123 @@ export default function Sidebar() {
   const buyerSidebarMenuList = [
     {
       menuIcon: <VendorNearMeIcon />,
-      menuName: "Seller Near Me",
-      linkTo: "/home",
-      handleClick: "",
+      menuName: 'Seller Near Me',
+      linkTo: '/home',
+      handleClick: '',
       authRequired: false,
     },
     {
       menuIcon: <StoreIcon />,
-      menuName: "My Store",
-      linkTo: "/seller",
-      handleClick: "",
+      menuName: 'My Store',
+      linkTo: '/seller',
+      handleClick: '',
       authRequired: true,
     },
     {
       menuIcon: <InboxIcon />,
-      menuName: "Inbox",
-      linkTo: "/user/inbox",
-      handleClick: "",
+      menuName: 'Inbox',
+      linkTo: '/user/inbox',
+      handleClick: '',
       authRequired: true,
     },
     {
       menuIcon: <MyFavoriteSeller />,
-      menuName: "My Favorite Seller",
-      linkTo: "/",
-      handleClick: "",
+      menuName: 'My Favorite Seller',
+      linkTo: '/',
+      handleClick: '',
       authRequired: true,
     },
     {
       menuIcon: <VoucherListIcon />,
-      menuName: "Voucher List",
-      linkTo: "/user/voucher",
-      handleClick: "",
+      menuName: 'Voucher List',
+      linkTo: '/user/voucher',
+      handleClick: '',
       authRequired: true,
     },
     {
       menuIcon: <LogoutIcon />,
-      menuName: "Logout",
-      linkTo: "/",
-      handleClick: "",
+      menuName: 'Logout',
+      linkTo: '/',
+      handleClick: '',
       authRequired: true,
     },
   ];
   const sellerSidebarMenuList = [
     {
       menuIcon: <CalendarIcon />,
-      menuName: "My Created Events",
-      linkTo: "/seller/createdevent",
-      handleClick: "",
+      menuName: 'My Created Events',
+      linkTo: '/seller/createdevent',
+      handleClick: '',
     },
     {
       menuIcon: <StoreIcon />,
-      menuName: "My Shop",
-      linkTo: "/seller/myshop",
-      handleClick: "",
+      menuName: 'My Shop',
+      linkTo: '/seller/myshop',
+      handleClick: '',
     },
     {
       menuIcon: <InboxIcon />,
-      menuName: "Inbox",
-      linkTo: "/seller/inbox",
-      handleClick: "",
+      menuName: 'Inbox',
+      linkTo: '/seller/inbox',
+      handleClick: '',
     },
     {
       menuIcon: <VoucherListIcon />,
-      menuName: "Offer Voucher List",
-      linkTo: "/seller/voucher",
-      handleClick: "",
+      menuName: 'Offer Voucher List',
+      linkTo: '/seller/voucher',
+      handleClick: '',
     },
   ];
-
+  const adminSidebarMenuList = [
+    {
+      menuIcon: <UserSummary />,
+      menuName: 'User Summary',
+      linkTo: '/',
+      handleClick: '',
+      authRequired: false,
+    },
+    {
+      menuIcon: <StoreIcon />,
+      menuName: 'Seller',
+      linkTo: '/',
+      handleClick: '',
+      authRequired: true,
+    },
+    {
+      menuIcon: <BuyerIcon />,
+      menuName: 'Buyer',
+      linkTo: '/',
+      handleClick: '',
+      authRequired: true,
+    },
+    {
+      menuIcon: <InboxInbox />,
+      menuName: 'Inbox',
+      linkTo: '/',
+      handleClick: '',
+      authRequired: true,
+    },
+    {
+      menuIcon: <ReportIcon />,
+      menuName: 'Requested Report',
+      linkTo: '/',
+      handleClick: '',
+      authRequired: true,
+    },
+  ];
   return (
     <div className="hidden xl:flex w-64 min-h-screen bg-white">
       <ul className="mt-3 z-[1] p-2 w-full shadow menu menu-sm dropdown-content bg-white rounded-box ">
-        {pathname.startsWith("/seller")
+        {pathname.startsWith('/seller')
           ? sellerSidebarMenuList.map((sidebarMenu) => (
+              <SidebarMenu
+                menuIcon={sidebarMenu.menuIcon}
+                menuName={sidebarMenu.menuName}
+                linkTo={sidebarMenu.linkTo}
+              />
+            ))
+          : pathname.startsWith('/admin')
+          ? adminSidebarMenuList.map((sidebarMenu) => (
               <SidebarMenu
                 menuIcon={sidebarMenu.menuIcon}
                 menuName={sidebarMenu.menuName}
