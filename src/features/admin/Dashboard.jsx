5158d45552423fdb99374f2e8ba4831c555c393a
SiraphobCh <spcc.321@gmail.com>
2024-07-02 11:04:43 +0700

    fix icons index file and update admin dashboard

--- src/features/admin/Dashboard.jsx
+++ src/features/admin/Dashboard.jsx
@@ -1,3 +1,16 @@
+import { BuyerIcon } from "../../icons";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div className="flex flex-col gap-10 justify-center items-center m-10">
+      <div className="w-full h-[250px] bg-green-300 rounded-lg flex flex-row justify-center items-center">
+        <div className="w-full h-[210px] mx-5 bg-red-200">
+          <div className="text-center text-2xl font-bold mt-2">User Breakdown</div>
+        </div>
+        <div className="w-full h-[210px] mx-5 bg-purple-200">BBB</div>
+      </div>
+      <div className="w-full h-[300px] bg-red-300">CCC</div>
+      <div className="w-full h-[300px] bg-blue-300">DDD</div>
+    </div>
+  );
 }
