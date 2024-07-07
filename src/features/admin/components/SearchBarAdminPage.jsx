3b3ce40db3e6130fd89c9c09496cd77aab02d9a8
SiraphobCh <spcc.321@gmail.com>
2024-07-07 14:45:23 +0700

    finish search bar and pagination first version

--- src/features/admin/components/SearchBarAdminPage.jsx
+++ src/features/admin/components/SearchBarAdminPage.jsx
@@ -1,14 +1,15 @@
+/* eslint-disable react/prop-types */
 import MagnifierSvg from "../../../icons/MagnifierSvg";
 
 export default function SearchBarAdminPage({ placeholder, searchQuery, handleSearch }) {
   return (
-    <div className="flex justify-center w-[550px]">
+    <div className="flex justify-center w-full">
       <div className="relative w-full">
         <input
           type="text"
           value={searchQuery}
           onChange={handleSearch}
-          className="w-full py-3 pl-3 pr-10 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent bg-absolutewhite"
+          className="w-full py-3 pl-3 pr-10 border rounded-2xl shadow-sm focus:outline-none focus:border-transparent bg-absolutewhite"
           placeholder={placeholder}
         />
         <div className="absolute inset-y-0 right-0 flex items-center pr-3">
