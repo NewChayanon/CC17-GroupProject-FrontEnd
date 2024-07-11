8771329d41e20fe4c1f4d1348f199996e373b09d
vtommino <vasindevwork@gmail.com>
2024-07-11 17:08:44 +0700

    updated mystore reviews

--- src/features/seller/MyStoreReviews.jsx
+++ src/features/seller/MyStoreReviews.jsx
@@ -1,29 +1,4 @@
-export function ReviewMock() {
-  return (
-    <>
-      <div className="w-full ">
-        <div className="flex justify-between w-full">
-          <div>"Exceptional Durian Experience!"</div>
-          <div>OOOOO</div>
-        </div>
-        <div className="text-sm p-1">
-          I had an amazing experience with khun Thepparin. The durians were
-          incredibly fresh and flavorful, straight from the Rayong farm. The
-          quality and taste were outstanding. Highly recommend for all durian
-          lovers!
-        </div>
-        <div className="flex justify-between">
-          <div className="text-primary font-bold">Supaporn Phantavee</div>
-          <div className="text-graydarktext font-semibold"> coupon used O</div>
-        </div>
-        <div className="flex text-sm pb-5">
-          <div className="text-sm">Reviewed on</div> <div>23 June 2024</div>
-        </div>
-        <hr />
-      </div>
-    </>
-  );
-}
+import ReviewTab from "./components/ReviewTab";
 
 export default function MyStoreReviews() {
   return (
@@ -32,12 +7,12 @@ export default function MyStoreReviews() {
         <div className="text-primary font-bold text-2xl">
           Review from Buyers
         </div>
-        <ReviewMock />
-        <ReviewMock />
-        <ReviewMock />
-        <ReviewMock />
-        <ReviewMock />
-        <ReviewMock />
+        <ReviewTab />
+        <ReviewTab />
+        <ReviewTab />
+        <ReviewTab />
+        <ReviewTab />
+        <ReviewTab />
       </div>
     </>
   );
