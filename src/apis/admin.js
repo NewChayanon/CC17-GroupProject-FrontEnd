87921e70e56b51918da9482ac5a9d67a06136fa2
Sirawit44 <164641741+Sirawit44@users.noreply.github.com>
2024-07-11 18:33:28 +0700

    update date

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -6,5 +6,6 @@ adminApi.createMessage = (body) => axios.post('/admin/new-message',body)
 adminApi.allMessage = () => axios.get("/admin/all-messages")
 adminApi.allBuyer = (params)=> axios.get("/admin/buyer", {params})
 adminApi.allSeller = (params) => axios.get("/admin/seller",{params})
+adminApi.allReport = (params) => axios.get("/admin/all-report",{params})
 
 export default adminApi;
